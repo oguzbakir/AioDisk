a5273acb7432a13409b866c3b94e2546d6cb27bd
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-03-29 23:20:58 +0300

    Added json reader, readme.md and edited gitignore

--- core.py
+++ core.py
@@ -1,2 +1,18 @@
+import os
+import json
 import yadisk
 
+apiKeyTemplate = """{
+	"yadisk" : {
+		"application-id" : "",
+		"application-secret" : "",
+		"token" : ""
+	}
+}"""
+
+if(os.path.exists("api_keys.json")):
+	j = json.load(open("api_keys.json"))
+else:
+	f = open("api_keys.json","w")
+	print(apiKeyTemplate,file=f)
+	print("Api key file created. Please fill required fields and re-run script.")
