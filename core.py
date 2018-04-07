e8f959a25d909fca687d424c679ed36a0d86c8e8
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-04-07 16:06:12 +0300

    Added travis

--- core.py
+++ core.py
@@ -1,18 +1,28 @@
 from __future__ import print_function
 import httplib2
 import os
+import sys
 import json
+import argparse
 
+from optparse import OptionParser
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+
+
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE", metavar="FILE")
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()
+
 
 apiKeyTemplate = """{
     "yadisk" : {
@@ -23,25 +33,27 @@ apiKeyTemplate = """{
 }"""
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Drive API Python Quickstart'
 global j
 
+
 def initYadisk():
-    if(os.path.exists("api_keys.json")):
+    global j
+    if (os.path.exists("api_keys.json")):
         j = json.load(open("api_keys.json"))
     else:
-        f = open("api_keys.json","w")
-        print(apiKeyTemplate,file=f)
+        f = open("api_keys.json", "w")
+        print(apiKeyTemplate, file=f)
         print("Api key file created. Please fill required fields and re-run script.")
 
+
 def checkGoogleAPI():
-    if(os.path.exists("client_secret.json")):
+    if (os.path.exists("client_secret.json")):
         return 1
     else:
         return 0
 
+
 def get_credentials():
-    
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -53,22 +65,49 @@ def get_credentials():
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        credentials = tools.run_flow(flow, store)
+
         print('Storing credentials to ' + credential_path)
     return credentials
 
+
 def main():
+    gDrive = False
+    yaDisk = False
+    mega = False
+    print("""Cloud Status:
+    Google Drive: {0}
+    Yandex Disk: {1}
+    Mega: {2}
+1- Show Google Drive Panel
+2- Show Yandex Disk Panel
+3- Show Mega Panel
+""".format(gDrive,yaDisk,mega))
+    choice = input("Please enter a number\n")
+    if choice == "1":
+        # Show Google Drive Panel
+        if not checkGoogleAPI():
+            print("Google client_secret.json not found. Please check your configuration.")
+            main()
+        else:
+            print("I'm in")
+    elif choice == "2":
+        # Show Yandex Disk Panel
+        nothing = 1
+    elif choice == "3":
+        # Show Mega Panel
+        nothing = 1
+    else:
+        sys.exit(0)
+
+    '''
     initYadisk()
-    
+
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
 
-    results = service.files().list(fields="nextPageToken, files(id, name)").execute()
+    results = service.files().list(fields="nextPageToken, files(id, name)",pageSize=10).execute()
     items = results.get('files', [])
     if not items:
         print('No files found.')
@@ -76,9 +115,9 @@ def main():
         print('Files:')
         for item in items:
             print('{0} ({1})'.format(item['name'], item['id']))
+            '''
+
 
 if __name__ == '__main__':
-    if checkGoogleAPI() == 1:
-        main()
-    else:
-        print("client_secret.json file not found")
+    main()
+
